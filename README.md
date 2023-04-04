01bade9145b6a4529984fb1af9e94bc0cc08849f
yijinc <yijinc@qq.com>
2023-04-04 16:30:53 +0800

    chapter 01: getting-started

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # spring-boot-starter
 
 hello spring boot
+
+
+### 常用命令
+
+```bash
+
+# 打印了项目依赖的树状图
+$ mvn dependency:tree 
+
+# 启动应用程序
+$ mvn spring-boot:run
+
+# 打包，生成 target/myproject-0.0.1-SNAPSHOT.jar 文件
+$ mvn package
+
+# 使用 java -jar 命令来运行这个可执行jar程序
+$ java -jar target/myproject-0.0.1-SNAPSHOT.jar
+
+  .   ____          _            __ _ _
+ /\\ / ___'_ __ _ _(_)_ __  __ _ \ \ \ \
+( ( )\___ | '_ | '_| | '_ \/ _` | \ \ \ \
+ \\/  ___)| |_)| | | | | || (_| |  ) ) ) )
+  '  |____| .__|_| |_|_| |_\__, | / / / /
+ =========|_|==============|___/=/_/_/_/
+ :: Spring Boot ::  (v3.1.0-SNAPSHOT)
+....... . . .
+....... . . . (log output here)
+....... . . .
+........ Started MyApplication in 2.536 seconds (process running for 2.864)
+```
--- src/main/java/com/fish/myspringboot/MySpringBootApplication.java
+++ src/main/java/com/fish/myspringboot/MySpringBootApplication.java
@@ -2,12 +2,21 @@ package com.fish.myspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+// @RestController 和 @RequestMapping 注解是Spring MVC注解（它们不是Spring Boot特有的）
+@RestController // @RestController 注解告诉Spring将返回的结果字符串直接响应给客户端。
+@SpringBootApplication // 这个注解被称为元注解，它结合了 @SpringBootConfiguration、@EnableAutoConfiguration 和 @ComponentScan
 public class MySpringBootApplication {
 
+	@RequestMapping("/") // @RequestMapping 注解提供了 “routing” （路由）信息。 它告诉Spring，任何带有 / 路径的HTTP请求都应该被映射到 home 方法
+	String home() {
+		return "hello world";
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(MySpringBootApplication.class, args);
+		SpringApplication.run(MySpringBootApplication.class, args); // args 数组也被传入，这是命令行参数
 	}
 
 }

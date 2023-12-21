59f0fc09d9b5e7018f3eaec0f3363c7a763032e3
yijinc <yijinc@qq.com>
2023-12-21 19:06:25 +0800

    配置数据源

---
+++ framework/src/main/java/example/config/DataSourceConfig.java
@@ -0,0 +1,20 @@
+package example.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * 配置自定义数据源
+ * Spring Boot 会在任何需要 DataSource 的地方重用它，包括数据库初始化
+ * 通过 配置文件 application属性 spring.datasource 获取配置
+ * **/
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    public DataSource dataSource() {
+        return new DruidDataSource();
+    }
+}

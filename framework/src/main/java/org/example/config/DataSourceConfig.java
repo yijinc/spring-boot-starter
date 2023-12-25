1ab8f81cc0fdf7e2b402230f294a1ae94b905bcf
yijinc <yijinc@qq.com>
2023-12-25 15:22:20 +0800

    update package

--- framework/src/main/java/org/example/config/DataSourceConfig.java
+++ framework/src/main/java/org/example/config/DataSourceConfig.java
@@ -1,4 +1,4 @@
-package example.config;
+package org.example.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.context.annotation.Bean;
--- framework/src/main/java/org/example/config/RedisConfig.java
+++ framework/src/main/java/org/example/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package example.config;
+package org.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
